114368fded1bb9a70c32b86e3b9832cbcb7160a7
Max.Arkell <maxarkell@icloud.com>
2024-02-20 15:03:46 +0000

    add express lines

--- Insights/server.js
+++ Insights/server.js
@@ -6,8 +6,11 @@ const connection = require('./connection');
 const app = express();
 const port = process.env.PORT || 3000;
 
+//this is the express code, need to figure out what this does lol
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(session({secret:'ihopethisworksitwouldbecool', resave:false, saveUninitialised:false}));
-app.use(passport.initialise());
+app.use(passport.initialize());
 app.use(passport.session());
 
 const authRoutes = require('./routes/auth');
