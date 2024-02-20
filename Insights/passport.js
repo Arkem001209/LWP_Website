42eb01e6ecf2d66c30e29abbf5666f3a544d0c83
Max.Arkell <maxarkell@icloud.com>
2024-02-20 14:03:34 +0000

    Add mysql connection to passport

--- Insights/passport.js
+++ Insights/passport.js
@@ -1,5 +1,22 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'SuperUser',
+    password: 'LondonWall1234',
+    database: 'insights'
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+});
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
