d3f3fa502918503278341d78b4a60334a6e08131
Max.Arkell <maxarkell@icloud.com>
2024-02-20 14:38:08 +0000

    add passport connection to server

--- Insights/server.js
+++ Insights/server.js
@@ -1,4 +1,17 @@
 const mysql = require('mysql');
+const express = require('express');
+const passport = require('passport');
+const passportConfig = require('./passport');
+const session = require('express-session');
+const app = express();
+
+app.use(session({secret:'ihopethisworksitwouldbecool', resave:false, saveUninitialised:false}));
+app.use(passport.initialise());
+app.use(passport.session());
+
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'SuperUser',
@@ -12,4 +25,8 @@ connection.connect((err) => {
         return;
     }
     console.log('Connected to MySQL');
+});
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
 });
