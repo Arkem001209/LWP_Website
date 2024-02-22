349bc62fb6ab0a268505ab5c9c502cbb023e93b4
Max.Arkell <maxarkell@icloud.com>
2024-02-22 15:16:19 +0000

    make slideshow smaller, attempt to make pictures the same size

--- clients/slideshow.js
+++ clients/slideshow.js
@@ -1,13 +1,13 @@
+const slidesContainer = document.querySelector('.slides');
 const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
 function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${(i - index) * 100}%)`;
-  });
+  const slideWidth = slides[0].offsetWidth;
+  slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
 }
 
 setInterval(() => {
-  currentIndex = (currentIndex + 1) % slides.length;
+  currentIndex = (currentIndex + 1) % (slides.length - 1); // Excluding the last slide
   showSlide(currentIndex);
 }, 3000); // Change slide every 3 seconds
